8bc3a3be0e14569fa14fcd34de5df57471c78de7
johnny <johnny-peckover@live.co.uk>
2019-09-24 11:47:58 +0100

    parameters

---
+++ src/main/scala/Day1/Parameters.scala
@@ -0,0 +1,11 @@
+package Day1
+
+object Parameters extends App{
+
+
+  def paramTest(test:String): Unit = println(test)
+
+  paramTest("hello")
+
+  }
+
---
+++ src/main/scala/Day1/ReturnTypes.scala
@@ -0,0 +1,5 @@
+package Day1
+
+object ReturnTypes extends App{
+
+}
