4511be8803c657f21e28352820805dfcd81c5efe
johnny <johnny-peckover@live.co.uk>
2019-09-24 17:08:57 +0100

    iteration 3

---
+++ src/main/scala/Day1/FizzbuzzRecursion.scala
@@ -0,0 +1,7 @@
+package Day1
+
+object FizzbuzzRecursion extends App{
+
+
+
+}
---
+++ src/main/scala/Day1/IterationRecursion.scala
@@ -0,0 +1,24 @@
+package Day1
+
+//object Iteration extends App{
+//
+//  def iteration(str:String, int:Int):Unit = {
+//    for (i <- 1 to int) {
+//      println(str)
+//    }
+//  }
+//  iteration("Hi", 5)
+//
+//}
+
+object IterationRecursion extends App{
+
+  def iterationRecursion(str:String, int:Int):Unit = {
+
+    val array = (1 to int).toArray[Int]
+
+
+  }
+
+}
+
