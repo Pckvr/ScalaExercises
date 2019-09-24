266aaebd9aa50b4d3f610d7e4cc002fc8d363d7f
johnny <johnny-peckover@live.co.uk>
2019-09-24 16:10:51 +0100

    iteration2

--- src/main/scala/Day1/Iteration.scala
+++ src/main/scala/Day1/Iteration.scala
@@ -2,7 +2,7 @@ package Day1
 
 object Iteration extends App{
 
-  def iteration(str:String, int:Int) = {
+  def iteration(str:String, int:Int):Unit = {
     for (i <- 1 to int) {
       println(str)
     }
