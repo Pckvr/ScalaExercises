bfb607e5d8930dde54abc25d1c3adee1a6e91e5d
johnny <johnny-peckover@live.co.uk>
2019-09-25 13:14:39 +0100

    pattern matching 1 (not finished)

--- src/main/scala/Day2/PatternMatching1.scala
+++ src/main/scala/Day2/PatternMatching1.scala
@@ -1,7 +1,11 @@
-//package Day2
-//
-//object PatternMatching1 extends App{
-//  def patternMatching1(int1:Int, int2:Int, check:Boolean) = {
-//    case
-//  }
-//}
+package Day2
+
+object PatternMatching1 extends App{
+  def patternMatching1(int1:Int, int2:Int, check:Boolean) = check match{
+    case check => println(int1 + int2)
+    case _ => println(int1 * int2)
+  }
+  patternMatching1(4,6, true)
+  patternMatching1(4,6,false)
+
+}
