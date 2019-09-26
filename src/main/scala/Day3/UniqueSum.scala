412022b07f6c1d814846c4c08acace4cf5d4b48f
johnny <johnny-peckover@live.co.uk>
2019-09-26 12:11:18 +0100

    unique sum one line

--- src/main/scala/Day3/UniqueSum.scala
+++ src/main/scala/Day3/UniqueSum.scala
@@ -3,15 +3,17 @@ package Day3
 object UniqueSum extends App{
 
   def uniqueSum(int1: Int, int2: Int, int3:Int) = {
-    val numbers = List(int1, int2, int3)
 
-    val duplicates = numbers diff numbers.distinct
+    val sum = (List(int1, int2, int3) filterNot (List(int1, int2, int3) diff List(int1, int2, int3).distinct).contains).sum
+//    val numbers = List(int1, int2, int3)
+//
+//    val duplicates = numbers diff numbers.distinct
+//
+//    val toBeAdded = numbers filterNot duplicates.contains
+//
+//    val sum = toBeAdded.sum
 
-    val toBeAdded = numbers filterNot duplicates.contains
-
-    val sum = toBeAdded.sum
-
-    println("List of numbers is " + numbers)
+    //println("List of numbers is " + numbers)
     println("The unique sum is " + sum)
 
   }
