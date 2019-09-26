9ddb5e46e374435a3bf4d2f4501a1dd2a0dedbb2
johnny <johnny-peckover@live.co.uk>
2019-09-26 14:42:57 +0100

    bike class

--- src/main/scala/Garage/Bike.scala
+++ src/main/scala/Garage/Bike.scala
@@ -1,5 +1,20 @@
 package Garage
 
-class Bike {
+class Bike  (
+  val id: Int,
+  val regNo: String,
+  val vehicleType: String,   //enum in future
+  val make: String,
+  val model: String,
+  val colour: String,
+  val customerID: Int,
+            ) extends Vehicle {
+  override def toString: String =
+    s"""$regNo,
+       |$vehicleType,
+       |$make $model,
+       |$colour,
+       |$customerID
+       |""".stripMargin
 
 }
