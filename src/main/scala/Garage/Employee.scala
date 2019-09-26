efc321b406c60f275689b87e763a57b92611c78b
johnny <johnny-peckover@live.co.uk>
2019-09-26 14:39:31 +0100

    car class

--- src/main/scala/Garage/Employee.scala
+++ src/main/scala/Garage/Employee.scala
@@ -20,6 +20,3 @@ class Employee (
        |""".stripMargin
 
 }
-               )
-
-}
--- src/main/scala/Garage/Vehicle.scala
+++ src/main/scala/Garage/Vehicle.scala
@@ -2,6 +2,7 @@ package Garage
 
 abstract class Vehicle {
   val id: Int
+  val regNo: String
   val vehicleType: String    //enum in future
   val make: String
   val model: String
