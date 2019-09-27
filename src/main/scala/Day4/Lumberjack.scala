52b327a1ef918c7c0a6c8878c3093ec3119a94de
johnny <johnny-peckover@live.co.uk>
2019-09-27 16:26:12 +0100

    lumberjack complete

--- src/main/scala/Day4/Lumberjack.scala
+++ src/main/scala/Day4/Lumberjack.scala
@@ -2,25 +2,18 @@ package Day4
 
 object Lumberjack extends App {
 
-  def logPileSorter(logPiles: Array[Int], newLogs: Int, indexCount: Int = 0):Any = {
-
-
-
-    if (newLogs != 0) {
-      logPileSorter(logPiles, newLogs, indexCount = 0)
-      if (indexCount < logPiles.length) {
-        if (logPiles(indexCount) <= logPiles.min) {
-          logPiles.update(indexCount, logPiles(indexCount) + 1)
-          logPileSorter(logPiles, newLogs - 1, indexCount + 1)
-        }
-        else {
-          logPileSorter(logPiles, newLogs, indexCount + 1)
+  def logPileSorter(logPiles: Array[Int], newLogs: Int):Any = {
+    var logs = newLogs
+      var smallestPile = logPiles.min
+      for(pile <- 0 to logPiles.length-1) {
+        if(logs >0) {
+        if (logPiles(pile) == smallestPile) {
+          logPiles.update(pile, logPiles(pile)+1)
+          logs -= 1
         }
       }
-    }   else {
-      println(logPiles)
     }
+    logPiles.foreach(println)
   }
   logPileSorter(Array(1,1,1,2,1,3,1,4,1), 7)
-
 }
