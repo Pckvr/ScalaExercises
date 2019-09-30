bbca717d05ed1c4cfa023a57dfe7f1da6b6835fb
johnny <johnny-peckover@live.co.uk>
2019-09-30 10:50:56 +0100

    new vehicle functionality

--- src/main/scala/Garage/Garage.scala
+++ src/main/scala/Garage/Garage.scala
@@ -5,10 +5,18 @@ class Garage {
   //collection class
   //holds ids for vehicles, customers, employees
 
+  var vehicleList: Map[Int, Vehicle] = Map()
+  var employeeList: Map[Int, Employee] = Map()
+  var customerList: Map[Int, Customer] = Map()
 
-  var carsList: List[Int] = List()
-  var customersList: List[Int] = List()
-  var employeesList: List[Int] = List()
+  def newCar(regNo: String, make: String, model: String, colour: String, customerID: Int) = {
+    val car = new Car(regNo, make, model, colour, customerID)
+    vehicleList += (car.vehicleID -> car)
+  }
 
+  def newBike(regNo: String, make: String, model: String, colour: String, customerID: Int) = {
+    val bike = new Bike(regNo, make, model, colour, customerID)
+    vehicleList += (bike.vehicleID -> bike)
+  }
 
 }
