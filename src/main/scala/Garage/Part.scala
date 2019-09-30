f267134302c319851832ae595d5be9c124017276
Johnny Peckover <johnny-peckover@live.co.uk>
2019-09-30 22:20:57 +0100

    part class

---
+++ src/main/scala/Garage/Part.scala
@@ -0,0 +1,28 @@
+package Garage
+
+import scala.util.Random
+
+class Part (val name:String,
+            var broken: Boolean = Random.nextInt(100) match{
+              case _%10 == 0 => true
+              case _ => false
+            },
+            val cost: Double = Random.between(10.0, 500.0),
+            val labourHours: Double = Random.between(0.5, 10)) {
+
+  override def toString: String =
+    s"""$name
+       |$broken
+       |$cost
+       |$labourHours""".stripMargin
+
+}
+
+object Part {
+  val partsList: List[String] = List("front tyre(s)", "rear tyre", "windscreen", "engine", "mirrors", "radio", "headlights", "indicator lights", "brake pads", "chassis")
+
+  def generateVehicleParts(): Unit ={
+    var vehicleParts: Array[Part] = Array()
+    partsList.foreach(name => {vehicleParts :+ new Part(name)})
+  }
+}
