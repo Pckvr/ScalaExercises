e99da4148c679c7efe9fab8d81874dd072b03f41
johnny <johnny-peckover@live.co.uk>
2019-10-01 16:36:50 +0100

    mongo connection

--- build.sbt
+++ build.sbt
@@ -2,9 +2,14 @@ name := "ScalaExercises"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.8"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
 
 libraryDependencies += "org.mockito" % "mockito-core" % "2.7.22" % Test
+
+libraryDependencies ++= Seq(
+  "org.slf4j" % "slf4j-log4j12" % "1.7.25",
+  "org.mongodb.scala" %% "mongo-scala-driver" % "2.6.0"
+)
 
---
+++ src/main/scala/Garage/MongoConnection.scala
@@ -0,0 +1,30 @@
+package Garage
+
+import org.mongodb.scala._
+
+object MongoConnection extends App{
+
+  val mongoClientPath = "//localhost"
+  val databaseName = "garage"
+
+  def getConnection():MongoClient = {
+    val mongoClient = MongoClient("mongodb:" + mongoClientPath)
+    mongoClient
+  }
+
+  def closeCollection() = {
+    getConnection().close()
+  }
+
+  def getDatabase():MongoDatabase = {
+    val database = getConnection().getDatabase(databaseName)
+    database
+  }
+
+  def getCollection(collectionName: String) = {
+    val collection = getDatabase().getCollection(collectionName)
+    collection
+  }
+
+
+}
