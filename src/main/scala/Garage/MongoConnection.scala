3315644c6571b7be8eaa3ddff3a7696820235f1a
johnny <johnny-peckover@live.co.uk>
2019-10-01 16:53:51 +0100

    add and get all documents

--- src/main/scala/Garage/MongoConnection.scala
+++ src/main/scala/Garage/MongoConnection.scala
@@ -7,7 +7,7 @@ class MongoConnection {
   val mongoClientPath = "//localhost"
   val databaseName = "garage"
 
-  def getConnection():MongoClient = {
+  def getConnection(): MongoClient = {
     val mongoClient = MongoClient("mongodb:" + mongoClientPath)
     mongoClient
   }
@@ -16,14 +16,28 @@ class MongoConnection {
     getConnection().close()
   }
 
-  def getDatabase():MongoDatabase = {
+  def getDatabase(): MongoDatabase = {
     val database = getConnection().getDatabase(databaseName)
     database
   }
 
-  def getCollection(collectionName: String) = {
+  def getCollection(collectionName: String): MongoCollection[Document] = {
     val collection = getDatabase().getCollection(collectionName)
     collection
   }
+
+  def addDocument(document: Document, collectionName: String) = {
+    getCollection(collectionName).insertOne(document)
+      .subscribe(new Observer[Completed] {
+        override def onNext(result: Completed): Unit = println("Document has been inserted")
+        override def onError(e: Throwable): Unit = println(s"Failed ${e.getStackTrace.toString}")
+        override def onComplete(): Unit = println("Document(s) have been inserted")
+      })
+  }
+
+  def getAllDocuments(collectionName: String) = {
+    getCollection(collectionName).find()
+  }
+
 
 }
