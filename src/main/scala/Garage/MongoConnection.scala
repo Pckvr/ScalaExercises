4ff05103cf98b691b25ffd0bd9e16d25d2e57b2a
johnny <johnny-peckover@live.co.uk>
2019-10-02 09:35:52 +0100

    split MongoConnection into MongoCRUD

--- src/main/scala/Garage/MongoConnection.scala
+++ src/main/scala/Garage/MongoConnection.scala
@@ -23,39 +23,4 @@ class MongoConnection {
     getDatabase().getCollection(collectionName)
   }
 
-  def addDocument(document: Document, collectionName: String) = {
-    getCollection(collectionName).insertOne(document)
-      .subscribe(new Observer[Completed] {
-        override def onNext(result: Completed): Unit = println("Document has been inserted")
-        override def onError(e: Throwable): Unit = println(s"Failed ${e.getStackTrace.toString}")
-        override def onComplete(): Unit = println("Document(s) have been inserted")
-      })
-  }
-
-  def getAllDocuments(collectionName: String) = {
-    getCollection(collectionName).find()
-  }
-
-  def getFirstDocument(collectionName: String) = {
-    getCollection(collectionName).find().head()
-  }
-
-//  def deleteById(collectionName: String, id: Int) = collectionName match {
-//    case "Employee" | "Customer" => {
-//      getCollection(collectionName).deleteOne(equal("id", id)).headOption().onComplete {
-//        onCompleteSuccessFailure()
-//      }
-////    }
-//    case "Vehicle" | "Car" | "Bike" => {
-//      getCollection(collectionName).deleteOne(equal("vehicleID", id)).headOption().onComplete {
-//        onCompleteSuccessFailure()
-//      }
-//}
-//  }
-//  def onCompleteSuccessFailure(): Unit = {
-//    case Success(value) => println("Successful")
-//    case Failure(error) => error.printStackTrace()
-//  }
-
-
 }
