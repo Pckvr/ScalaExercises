3679cc852feb1d6b7bce26829a9e2c86e64a3073
johnny <johnny-peckover@live.co.uk>
2019-10-02 10:21:55 +0100

    MongoConnection Before

--- src/test/scala/Garage/MongoConnectionTests.scala
+++ src/test/scala/Garage/MongoConnectionTests.scala
@@ -4,5 +4,12 @@ import resources.BaseTestingClass
 
 class MongoConnectionTests extends BaseTestingClass {
 
+  var mongoConnection: MongoConnection = _
+
+  before {
+    mongoConnection = new MongoConnection
+  }
+
+
 
 }
