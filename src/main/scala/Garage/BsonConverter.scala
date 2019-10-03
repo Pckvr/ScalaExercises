c0794d5b8c45f68a294b019aebd51c7291a73108
johnny <johnny-peckover@live.co.uk>
2019-10-03 10:36:11 +0100

    tests pass

--- src/main/scala/Garage/BsonConverter.scala
+++ src/main/scala/Garage/BsonConverter.scala
@@ -3,7 +3,7 @@ package Garage
 import org.mongodb.scala.bson.collection.immutable.Document
 import org.mongodb.scala.bson._
 
-object BsonConverter extends App{
+class BsonConverter {
   def bsonTransformer[T](value: T)(implicit transformer: BsonTransformer[T]):BsonValue= {
     transformer(value)
   }
@@ -18,8 +18,8 @@ object BsonConverter extends App{
       case (key, value:Boolean) => (key, bsonTransformer(value))
       case (key, value:Seq[Int]) => (key, bsonTransformer(value))
       case (key, value:Seq[String]) => (key, bsonTransformer(value))
-      //case (key, value:Seq[Product]) => (key, objectToDocument(value.foreach))
       case (key, value:Product) => (key, objectToDocument(value))
+      //case (key, value:Seq[Product]) => (key, objectToDocument(value.foreach))
       case (key, value:Any) => (key, bsonTransformer(value.toString))
 
       // need to add a better list handling case
@@ -37,17 +37,17 @@ object BsonConverter extends App{
 
   //parts [LGarage.Part;@548a9f61
 
-  val garage = new Garage
-
-  var testCar = garage.newCar("AB12 CDE", "Fiat", "Punto", "Black", 1)
-  var testBike = garage.newBike("AB12 CDE", "Ducatti", "Something", "Red", 1)
-  var testEmployee = garage.registerEmployee("Mr", "Pete", "Smith", 1, "Test Street", "Test Town", "Test City", "T3 5TY", "012345678", "test@test.com", "Mechanic" )
-  var testCustomer = garage.registerCustomer("Mr", "Simon", "Jones", 2, "Test Street", "Test Town", "Test City", "T3 5TY", "012345678", "test@test.com")
+//  val garage = new Garage
 //
-
-  println(objectToDocument(testCar))
-  println(objectToDocument(testBike))
-  println(objectToDocument(testEmployee))
-  println(objectToDocument(testCustomer))
+//  var testCar = garage.newCar("AB12 CDE", "Fiat", "Punto", "Black", 1)
+//  var testBike = garage.newBike("AB12 CDE", "Ducatti", "Something", "Red", 1)
+//  var testEmployee = garage.registerEmployee("Mr", "Pete", "Smith", 1, "Test Street", "Test Town", "Test City", "T3 5TY", "012345678", "test@test.com", "Mechanic" )
+//  var testCustomer = garage.registerCustomer("Mr", "Simon", "Jones", 2, "Test Street", "Test Town", "Test City", "T3 5TY", "012345678", "test@test.com")
+////
+//
+//  println(objectToDocument(testCar))
+//  println(objectToDocument(testBike))
+//  println(objectToDocument(testEmployee))
+//  println(objectToDocument(testCustomer))
 
 }
